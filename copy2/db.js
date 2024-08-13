c2ce1210b7d834dd83651093aa4c57b2e12dd643
Ayush <ayushgarg678@gmail.com>
2024-08-13 21:30:36 +0530

    Initial commit

--- copy2/db.js
+++ copy2/db.js
@@ -4,7 +4,6 @@ const connectDb = async ()=>{
       console.log("db connected.......");
       await mongoose.connect("mongodb+srv://ayushgarg678:si8h57IMUD5yJXoO@cluster0.ygj3g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");  
 
-      //await mongoose.connect("mongodb+srv://chirag2306garg:Et1kF7lFs2E7hExo@cluster0.vf0aepw.mongodb.net/?retryWrites=true&w=majority");  
     } catch (error) {
         console.log(error);
     }
